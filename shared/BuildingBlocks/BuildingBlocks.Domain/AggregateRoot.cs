2a83ad99de13b6704f2933df66ffe403fc45ec1d
angpoginiko <bglascano@gmail.com>
2026-01-31 10:24:05 +0800

    added building blocks project

---
+++ shared/BuildingBlocks/BuildingBlocks.Domain/AggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Domain
+{
+    public class AggregateRoot
+    {
+
+    }
+}
---
+++ shared/BuildingBlocks/BuildingBlocks.Messaging/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Messaging
+{
+    public class IntegrationEvent
+    {
+
+    }
+}
